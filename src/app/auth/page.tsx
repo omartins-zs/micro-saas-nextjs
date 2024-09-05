ef1dedf186cf36ca893c15a9a5d0006c475ba1a9
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 10:23:16 -0300

    Fazendo Redirect em Usuario Logado pela Session

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,11 @@
+import { auth } from "@/services/auth";
 import { AuthForm } from "./_components/auth-form";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <AuthForm />
-    </main>
-  )
+export default async function Page() {
+
+  const session = await auth()
+  if (session?.user) return redirect('/app')
+  return <AuthForm />
+
 }
