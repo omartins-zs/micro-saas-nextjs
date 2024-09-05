da429a53362075177b17e5f472d7d4869fdb6d5d
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-05 10:54:53 -0300

    Tirando Verificação após middleware

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,5 @@
-import { auth } from "@/services/auth";
 import { AuthForm } from "./_components/auth-form";
-import { redirect } from "next/navigation";
 
-export default async function Page() {
-
-  const session = await auth()
-  if (session?.user) return redirect('/app')
+export default function Page() {
   return <AuthForm />
-
 }
