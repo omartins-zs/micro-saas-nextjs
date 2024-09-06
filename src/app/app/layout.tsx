acf02691aaf9cb3a432eace9bade9d1aa41f007a
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-06 09:36:20 -0300

    Importando Sidebar Padrão no Layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,10 +1,20 @@
+import { Sidebar } from "@/components/dashboard/sidebar";
 import { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
-        <div>
-            <header>Teste</header>
-            {children}
+        <div className="grid grid-cols-[16rem_1fr] gap-4">
+            <Sidebar
+                paths={[
+                    {
+                        href: '/',
+                        label: 'Tarefas'
+                    }
+                ]}
+            />
+            <main>
+                {children}
+            </main>
         </div>
     )
 }
