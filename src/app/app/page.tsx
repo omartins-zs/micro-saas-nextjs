4f6663f3e2cf4cfd51d819af13edef3fc1f31782
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-06 16:29:21 -0300

    Adicionando DashboardPage no Page do App

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,13 +1,19 @@
-
-import { auth } from "@/services/auth"
-import { UserInfo } from "./_components/user-info"
+import {
+  DashboardPage,
+  DashboardPageHeader,
+  DashboardPageHeaderTitle,
+  DashboardPageMain,
+} from '@/components/dashboard/page'
 
 export default async function Page() {
-  const session = await auth()
-
   return (
-    <main className="flex items-center justify-center h-screen">
-      <UserInfo user={session?.user} />
-    </main>
+    <DashboardPage>
+      <DashboardPageHeader>
+        <DashboardPageHeaderTitle>Tarefas</DashboardPageHeaderTitle>
+      </DashboardPageHeader>
+      <DashboardPageMain>
+        <h1>Tarefas</h1>
+      </DashboardPageMain>
+    </DashboardPage>
   )
 }
