30b5468de7ef9ef6102d2743cf48b78d36bc7cd0
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-06 10:30:24 -0300

    Add os Componentes do Sidebar no Layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,17 +1,37 @@
-import { Sidebar } from "@/components/dashboard/sidebar";
+import { Sidebar, SidebarFooter, SidebarHeader, SidebarHeaderTitle, SidebarMain, SidebarNav, SidebarNavHeader, SidebarNavLink, SidebarNavMain } from "@/components/dashboard/sidebar";
 import { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
         <div className="grid grid-cols-[16rem_1fr] gap-4">
-            <Sidebar
-                paths={[
-                    {
-                        href: '/',
-                        label: 'Tarefas'
-                    }
-                ]}
-            />
+            <Sidebar>
+                <SidebarHeader>
+                    <h1></h1>
+                </SidebarHeader>
+                <SidebarMain>
+                    <SidebarNav>
+                        <SidebarNavMain>
+                            <SidebarNavLink href="/app">Tarefas</SidebarNavLink>
+                            <SidebarNavLink href="/app/settings">Configurações</SidebarNavLink>
+                        </SidebarNavMain>
+                    </SidebarNav>
+                    <SidebarNav>
+                        <SidebarNavHeader>
+                            <SidebarHeaderTitle>
+                                Links Extras
+                            </SidebarHeaderTitle>
+                        </SidebarNavHeader>
+                        <SidebarNavMain>
+                            <SidebarNavLink href="/">Precisa de Ajuda?</SidebarNavLink>
+                            <SidebarNavLink href="/">Site</SidebarNavLink>
+                        </SidebarNavMain>
+                    </SidebarNav>
+                </SidebarMain>
+                <SidebarFooter>
+                    <h1>USer</h1>
+                </SidebarFooter>
+            </Sidebar>
+
             <main>
                 {children}
             </main>
