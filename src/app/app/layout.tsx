c6c10decc5f09e2ca4db0fadcf9fe8ffd336f900
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-06 12:19:33 -0300

    Add o MainSidebar no Layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,8 +1,10 @@
 import { PropsWithChildren } from "react";
+import { MainSidebar } from "./_components/main-sidebar";
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
         <div className="grid grid-cols-[16rem_1fr] gap-4">
+            <MainSidebar />
             <main>
                 {children}
             </main>
