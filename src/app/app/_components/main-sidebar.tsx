3bfcd0e76ab0a8ae6411a4ae880e9565ec6e7fca
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-06 14:57:23 -0300

    Add User Dropdown e Logo no Sidebar

--- src/app/app/_components/main-sidebar.tsx
+++ src/app/app/_components/main-sidebar.tsx
@@ -13,6 +13,8 @@ import {
 } from "@/components/dashboard/sidebar";
 import { HomeIcon, MixerVerticalIcon } from "@radix-ui/react-icons";
 import { usePathname } from "next/navigation";
+import { UserDropdown } from "./user-dropdown";
+import { Logo } from "@/components/logo";
 
 export function MainSidebar() {
   const pathname = usePathname()
@@ -24,7 +26,7 @@ export function MainSidebar() {
   return (
     <Sidebar>
       <SidebarHeader>
-        <h1></h1>
+        <Logo />
       </SidebarHeader>
       <SidebarMain className="flex flex-col flex-grow">
         <SidebarNav>
@@ -49,7 +51,7 @@ export function MainSidebar() {
         </SidebarNav>
       </SidebarMain>
       <SidebarFooter>
-        <h1>User</h1>
+        <UserDropdown />
       </SidebarFooter>
     </Sidebar>
   );
