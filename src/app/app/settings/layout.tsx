772cdc0f6d1cc03cc4639cd7aa49649d46ee7023
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-27 16:08:26 -0300

    Ajustando o Layout do Settings

--- src/app/app/settings/layout.tsx
+++ src/app/app/settings/layout.tsx
@@ -1,22 +1,26 @@
-import { DashboardPage, DashboardPageHeader, DashboardPageHeaderTitle, DashboardPageMain} from "@/components/dashboard/page";
-import { PropsWithChildren } from "react";
-import { SettingsSidebar } from "./_components/settings-sidebar";
+import {
+  DashboardPage,
+  DashboardPageHeader,
+  DashboardPageHeaderTitle,
+  DashboardPageMain,
+} from '@/components/dashboard/page'
+import { PropsWithChildren } from 'react'
+import { SettingsSidebar } from './_components/settings-sidebar'
 
 export default function Layout({ children }: PropsWithChildren) {
-    return (
-        <DashboardPage>
-            <DashboardPageHeader>
-                <DashboardPageHeaderTitle>Configurações</DashboardPageHeaderTitle>
-            </DashboardPageHeader>
-            <DashboardPageMain>
-                <h1>Configurações</h1>
-            </DashboardPageMain>
-            <div className="grid grid-cols-[16rem_1fr] gap-12">
-              <SettingsSidebar />
-                <div>{children}</div>
-            </div>
-        </DashboardPage>
-    )
-
-
+  return (
+    <DashboardPage>
+      <DashboardPageHeader>
+        <DashboardPageHeaderTitle>Configurações</DashboardPageHeaderTitle>
+      </DashboardPageHeader>
+      <DashboardPageMain>
+        <div className="container max-w-screen-lg">
+          <div className="grid grid-cols-[10rem_1fr] gap-12">
+            <SettingsSidebar />
+            <div>{children}</div>
+          </div>
+        </div>
+      </DashboardPageMain>
+    </DashboardPage>
+  )
 }
