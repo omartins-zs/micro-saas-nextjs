1efade80675cce70042fa8ad7d010ca27b853d41
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-27 16:10:48 -0300

    Add Component Form do Settings e Edit de Name

---
+++ src/app/app/settings/(main)/_components/form.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { updateProfile } from '../actions'
+import { updateProfileSchema } from '../schemas'
+import { toast } from '@/hooks/use-toast'
+import { z } from 'zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { SheetFooter } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Session } from 'next-auth'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+type ProfileFormProps = {
+  defaultValues: Session['user']
+}
+
+export function ProfileForm({ defaultValues }: ProfileFormProps) {
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+    defaultValues: {
+      name: defaultValues?.name ?? '',
+      email: defaultValues?.email ?? '',
+    },
+  })
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    await updateProfile(data)
+    router.refresh()
+
+    toast({
+      title: 'Success',
+      description: 'Your profile has been updated successfully.',
+    })
+  })
+
+  return (
+    <Form {...form}>
+      <form onSubmit={onSubmit} className="space-y-8">
+        <Card>
+          <CardHeader>
+            <CardTitle>Name</CardTitle>
+            <CardDescription>
+              Este será o nome exibido publicamente.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Email</CardTitle>
+            <CardDescription>
+             Please contact email contact@micro-saas.com to change the email.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" readOnly {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+
+        <SheetFooter className="mt-auto">
+          <Button disabled={form.formState.isLoading} type="submit">
+            {form.formState.isSubmitting && 'Salvando...'}
+            {!form.formState.isSubmitting && 'Save changes'}
+          </Button>
+        </SheetFooter>
+      </form>
+    </Form>
+  )
+}
