77c18abf094dc2b49f3b76066ed84868597740f7
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-02 07:39:58 -0300

    Add Config do Stripe

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    stripe: {
+      publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+      secretKey:  process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
+      proPriceId: '',
+      webhookSecret: ''
+    }
+  }
