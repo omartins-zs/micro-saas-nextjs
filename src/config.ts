a3b286d63ad2c962a467d3418ca9bf9784177f3a
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-02 08:12:12 -0300

    Add Configuração dos Planos do Stripe

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,23 @@
 export const config = {
-    stripe: {
-      publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-      secretKey:  process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
-      proPriceId: '',
-      webhookSecret: ''
-    }
+  stripe: {
+    publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    secretKey: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
+    webhookSecret: '',
+    plans: {
+      free: {
+        stripeId: "price_1Q5QGRD86tsvbg9Ztk0d9DQJ",
+        quota: {
+          TASKS: 5
+        }
+      },
+      pro: {
+        stripeId: "price_1Q5QGfD86tsvbg9Z4ZLhR6WL",
+        quota: {
+          // Task -1 = Ilimitado
+          TASKS: 100
+        }
+      },
+    },
+    proPriceId: '',
   }
+}
