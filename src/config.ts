e5978203e7621e5b3e2893cf4d95d3f9d19fef67
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-02 14:16:29 -0300

    Add Id dos Planos no Stripe

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,13 @@ export const config = {
     webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
     plans: {
       free: {
-        stripeId: "price_1Q5QGRD86tsvbg9Ztk0d9DQJ",
+        priceId: "price_1Q5QGRD86tsvbg9Ztk0d9DQJ",
         quota: {
           TASKS: 5
         }
       },
       pro: {
-        stripeId: "price_1Q5QGfD86tsvbg9Z4ZLhR6WL",
+        priceId: "price_1Q5QGfD86tsvbg9Z4ZLhR6WL",
         quota: {
           // Task -1 = Ilimitado
           TASKS: 100
