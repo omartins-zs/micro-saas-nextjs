dd17d123c25b5e44e4233939b98580dde3464b8d
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-02 08:13:40 -0300

    Implementando a Session e Checkout da Action

--- src/app/app/settings/billing/actions.ts
+++ src/app/app/settings/billing/actions.ts
@@ -1,6 +1,27 @@
 'use server'
 
+import { auth } from '@/services/auth'
+import { createCheckoutSession } from '@/services/stripe'
+import { redirect } from 'next/navigation'
 
 export async function createCheckoutSessionAction() {
-  console.log("123")
+  // console.log("123")
+  // console.log({ok: true})
+
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return {
+      error: 'Not authorized',
+      data: null,
+    }
+  }
+
+  const checkoutSession = await createCheckoutSession(
+    session.user.id as string,
+    session.user.email as string,
+  )
+
+  if (!checkoutSession.url) return
+  redirect(checkoutSession.url)
 }
